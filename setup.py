ead4dff2b30f74b2dc1fea2b165ab43272e257e4
Ian McCracken <ian@zenoss.com>
2010-09-03 23:41:29 -0500

    Added egg structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='txrestapi',
+      version=version,
+      description="Easing the creation of REST API services in Python",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Ian McCracken',
+      author_email='ian.mccracken@gmail.com',
+      url='http://github.com/iancmcc/txrestapi',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
